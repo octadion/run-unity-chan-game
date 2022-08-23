7b06d9da2312898db2b1dfc83b17115ec71f682a
octadion <octadion@student.uns.ac.id>
2022-08-23 11:48:15 +0700

    initial commit

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public AudioSource levelMusic;
+    public GameObject pauseMenu;
+    public AudioSource pause;
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (gamePaused == false)
+            {
+                pause.Play();
+                pauseMenu.SetActive(true);
+                levelMusic.Pause();
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+                levelMusic.UnPause();
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        levelMusic.UnPause();
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+        
+    }
+    public void RestartLevel()
+    {
+        pauseMenu.SetActive(false);
+        levelMusic.UnPause();
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(2);
+    }
+    public void QuitToMenu()
+    {
+        pauseMenu.SetActive(false);
+        levelMusic.UnPause();
+        Cursor.visible = true;
+        gamePaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/ScrollCredit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScrollCredit : MonoBehaviour
+{
+    public GameObject creditsRun;
+     void Start()
+    {
+        StartCoroutine(RollCreds());
+    }
+    IEnumerator RollCreds()
+    {
+        yield return new WaitForSeconds(0.5f);
+        creditsRun.SetActive(true);
+        yield return new WaitForSeconds(12);
+        SceneManager.LoadScene(1);
+    }
+}
